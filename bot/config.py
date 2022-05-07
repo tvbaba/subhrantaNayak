f73ef146850b9728ba90c52d02054f3db380052f
tvbaba <104225271+tvbaba@users.noreply.github.com>
2022-05-07 08:48:15 +0500

    New

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,10 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1322549723
     OWNER = config("OWNER")
-    ffmpegcode = ["-preset faster -c:v libx265 -s 852x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://t.me/animecolony)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
+    FFMPEG = config(
+        "FFMPEG",
+        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
+    )
     THUMB = config("THUMBNAIL")
 except Exception as e:
     LOGS.info("Environment vars Missing")
